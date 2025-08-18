3460bf41a0b3c0aed6bb47b77e9a68709da6309d
Ankit Sharma <ankitsharma8102002@gmail.com>
2025-08-18 06:22:34 +0530

    Change in question

--- JS_InterviewQuestion/Q1.js
+++ JS_InterviewQuestion/Q1.js
@@ -1,7 +1,7 @@
-const person={
-    name:"Ankit",
-    age:22,
-    great: function(){
+const person = {
+    name: "Ankit",
+    age: 22,
+    great: function () {
         console.log("Hello, I am ", this.name);
     }
 }
@@ -10,5 +10,5 @@ person.great();
 // Yes , JavaScript object can hold a function as a property
 // we can function like this , person.great();
 
-//Output 
+//Output
 // Hello, I am  Ankit
